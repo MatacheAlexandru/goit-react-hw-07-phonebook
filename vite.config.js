94a51720b05c6361b17e8065f6f5c0c314ef274a
MatacheAlexandru <111695999+MatacheAlexandru@users.noreply.github.com>
2024-10-06 20:26:49 +0300

    4

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   base: "/goit-react-hw-07-phonebook/", // numele repo-ului tău GitHub
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["axios"], // adaugă axios aici ca dependență externă
-    },
-  },
 });
