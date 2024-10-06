3b10c332f079daaec2380d612be6bbe7c8737faf
MatacheAlexandru <111695999+MatacheAlexandru@users.noreply.github.com>
2024-10-06 20:08:32 +0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/goit-react-hw-06-phonebook/", // numele repo-ului tău GitHub
+  base: "/goit-react-hw-07-phonebook/", // numele repo-ului tău GitHub
   plugins: [react()],
   server: {
     proxy: {
